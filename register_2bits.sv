f9d9d6ff76c5dc64e564183a6026d37e4dab8f58
mariarivasb526-eng <maria.rivas.b526@gmail.com>
2025-08-28 17:04:25 -0600

    Update register_2bits.sv
    modificar din1 por zero y din2 por error en el TB

--- register_2bits.sv
+++ register_2bits.sv
@@ -2,7 +2,7 @@ module register_bank #(parameter WIDTH = 8)
 	(input clk,
 	input rst,
 	input wr_en,
-	input [WIDTH-1:0] din1, din2, 
+	input [WIDTH-1:0] zero, error,  //din1 se modifico por zero y din2 se modifico por error
 	output [WIDTH-1:0] q1, q2);
 
 always @(posedge clk)begin
@@ -11,8 +11,9 @@ always @(posedge clk)begin
 		q2 <= 0;
 	end
 	else if (wr_en) begin
-		q1 <= din1;
-		q2 <= din2;
+		q1 <= zero;
+		q2 <= error;
 	end
 end	
+
 endmodule
