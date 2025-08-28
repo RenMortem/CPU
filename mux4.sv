1b51558ba6e1a1b22134d88bdedcc076ecd776cb
Sara Neftalí Martínez García <79623749+Sara-space@users.noreply.github.com>
2025-08-27 23:50:32 -0600

    Create mux4.sv
    Migración de archivo mux4.v a mux4.sv

---
+++ mux4.sv
@@ -0,0 +1,19 @@
+module mux4 #(
+  parameter int WIDTH = 8
+) (
+  input  logic [WIDTH-1:0] din1, din2, din3, din4,
+  input  logic [1:0]       sel,
+  output logic [WIDTH-1:0] dout
+);
+
+always_comb begin
+  case (sel)
+    2'b00:   dout = din1;
+    2'b01:   dout = din2;
+    2'b10:   dout = din3;
+    2'b11:   dout = din4;
+    default: dout = din1; 
+  endcase
+end
+
+endmodule
