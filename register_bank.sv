f4727c0a1ac9880c565f2ff6be839fa03c52c80e
MariaRivas-lab <refugiorivas679@gmail.com>
2025-08-28 00:16:14 -0600

    Add files via upload

---
+++ register_bank.sv
@@ -0,0 +1,16 @@
+module register_bank #(
+	parameter WIDTH = 8
+)(
+	input clk,
+	input reset,
+	input enable,
+	input [WIDTH-1:0] inD,
+	output reg [WIDTH-1:0] outQ
+);
+	always @(posedge clk) begin //flanco de subida del clk
+		if (reset) begin    //comienza reset (activo)
+			outQ <= 0;  //la salida del regbank es 0
+		end else if (enable)
+			outQ <= inD;
+	end
+endmodule 
