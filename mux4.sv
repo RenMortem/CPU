c219744e43e88a2bc632d2a3fed56e6fc761d47f
RenMortem <gumballo1@outlook.com>
2025-08-29 15:41:47 -0600

    Update mux4.sv
    se cambió el nombre del puerto "sel" a "select" para que coincida con el cpu_top

--- mux4.sv
+++ mux4.sv
@@ -2,12 +2,12 @@ module mux4 #(
   parameter int WIDTH = 8
 ) (
   input  logic [WIDTH-1:0] din1, din2, din3, din4,
-  input  logic [1:0]       sel,
+  input  logic [1:0]       select,
   output logic [WIDTH-1:0] dout
 );
 
 always_comb begin
-  case (sel)
+  case (select)
     2'b00:   dout = din1;
     2'b01:   dout = din2;
     2'b10:   dout = din3;
